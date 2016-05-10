4952d456ff6f97053de68bd5311c6904120559ff
DashaBilogub <dasha.bilogub@mail.ru>
2016-05-10 19:25:19 +0300

    True input
    Changed creating_arbitrate_form.py for true input of password and login

--- untitled5/creating_arbitrate_form.py
+++ untitled5/creating_arbitrate_form.py
@@ -1,20 +1,27 @@
 from django import forms
 from django.forms import modelformset_factory
 from polls.models import *
+from django.utils.translation import gettext as _
 
 
 class PdnForm(forms.Form):
-    login = forms.CharField(max_length=20)
-    first_name = forms.CharField(max_length=15)
-    last_name = forms.CharField(max_length=15)
-    password = forms.CharField(max_length=30, widget=forms.PasswordInput())
-    repeat_password = forms.CharField(max_length=30, widget=forms.PasswordInput())
+    login = forms.CharField(label=_('Логін'),max_length=20)
+    first_name = forms.CharField(label=_('Ім\'я'),max_length=15)
+    last_name = forms.CharField(label=_('Прізвище'),max_length=15)
+    password = forms.CharField(label=_('Пароль'),max_length=30, widget=forms.PasswordInput())
+    repeat_password = forms.CharField(label=_('Повторіть пароль'),max_length=30, widget=forms.PasswordInput())
 
     def clean(self):
-        if self.cleaned_data.get('password') != self.cleaned_data.get('repeat_password'):
-            raise forms.ValidationError('Password did\'nt match')
-        else:
-            return self.cleaned_data
+        if self.cleaned_data.get('password') or self.cleaned_data.get('login') is not None:
+            if self.cleaned_data.get('password') != self.cleaned_data.get('repeat_password'):
+                raise forms.ValidationError('Пароль не співпадає')
+            elif len(self.cleaned_data.get('password')) < 5:
+                raise forms.ValidationError('Пароль має бути не менше 5 символів')
+            elif len(self.cleaned_data.get('login')) < 5:
+                raise forms.ValidationError('Логін має бути не менше 5 символів')
+            else:
+                return self.cleaned_data
+
 
 
 class CertForm(forms.ModelForm):
@@ -23,8 +30,13 @@ class CertForm(forms.ModelForm):
         fields = ('exam_complete_date', 'id_exam_protocol', 'date_certificate_mju', 'date_certificate', 'info_quality',
                   'full_number', 'working_exp', 'renewal_certificate', 'audit',)
         localized_fields = ('__all__',)
-        widgets = {'exam_complete_date': forms.SelectDateWidget(), 'date_certificate_mju': forms.SelectDateWidget(),
-                   'date_certificate': forms.SelectDateWidget(), 'info_quality': forms.SelectDateWidget()}
+        widgets = {'exam_complete_date': forms.SelectDateWidget(years=range(1990, 2017)),
+                   'date_certificate_mju': forms.SelectDateWidget(years=range(1990, 2017)),
+                   'date_certificate': forms.SelectDateWidget(years=range(1990, 2017)),
+                   'info_quality': forms.SelectDateWidget(years=range(1990, 2017))}
+
+
+
 
 
 class ArbitrateForm(forms.ModelForm):
@@ -33,7 +45,7 @@ class ArbitrateForm(forms.ModelForm):
         model = Arbitration
         fields = ('activity_info', 'dismissal_date', 'office_location', 'organization_field',
                   'name_register',)
-        widgets = {'dismissal_date': forms.SelectDateWidget()}
+        widgets = {'dismissal_date': forms.SelectDateWidget(years=range(1990, 2017))}
 
 
 
