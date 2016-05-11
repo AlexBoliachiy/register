baf1e5d04db5ac80a1cf2f92c8f80711431dc2c4
DashaBilogub <dasha.bilogub@mail.ru>
2016-05-11 20:41:17 +0300

    Modification of the site
    Added links for /arbitrates/
    Changed models.py for correct display of fields' name

--- polls/models.py
+++ polls/models.py
@@ -1,10 +1,6 @@
-from django.db import models
-
 # Create your models here.
-import datetime
 
 from django.db import models
-from django.utils import timezone
 from django.contrib.auth.models import User
 
 
@@ -15,13 +11,13 @@ class Department(models.Model):
 
 class Certificate(models.Model):
     exam_complete_date = models.DateField("Дата здачі екзамену")
-    id_exam_protocol = models.CharField("Номер екзаменацыйного протоколу",max_length=8)
+    id_exam_protocol = models.CharField("Номер екзаменацыйного протоколу", max_length=8)
     date_certificate_mju = models.DateField("Дата отримання сертифікату в МЮУ")
     date_certificate = models.DateField("Дата видачі сертифікату")
     info_quality = models.DateField("Дата отримання оцінки")
-    full_number = models.CharField("Номер сертифікату",max_length=13)
-    working_exp = models.IntegerField("Досвід роботи",default=0)
-    renewal_certificate = models.CharField("Відновлення сертифікату",max_length=64)
+    full_number = models.CharField("Номер сертифікату", max_length=13)
+    working_exp = models.IntegerField("Досвід роботи", default=0)
+    renewal_certificate = models.CharField("Відновлення сертифікату", max_length=64)
     audit = models.TextField("Аудит")
 
     def add(self, exam_complete_date, id_exam_protocol, date_certificate_mju, date_certificate, info_quality, full_number,
@@ -42,17 +38,18 @@ class Certificate(models.Model):
 
 
 class Arbitration(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, default=None)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=None,
+                                null=True, verbose_name='Арбітражний керуючий')
     certificate = models.ForeignKey(Certificate, on_delete=models.CASCADE)
     dismissal_date = models.DateField("Дата звільнення")
-    organization_field = models.CharField("Назва організації",max_length=64)
-    office_location = models.CharField("Розміщення офісу",max_length=6)
+    organization_field = models.CharField("Назва організації", max_length=64)
+    office_location = models.CharField("Розміщення офісу", max_length=6)
     activity_info = models.TextField("Інформація щодо діяльності")
-    name_register = models.CharField("Ім'я регістратора",max_length=64)
+    name_register = models.CharField("Ім'я регістратора", max_length=64)
     dep = models.ForeignKey(Department, default=None, on_delete=models.DO_NOTHING, null=True)
 
     def __str__(self):
-        return str(self.certificate)
+        return self.user.get_full_name()
 
     def add(self, acrivity_info, dismissal_date, office_location, organization_field, name_register,
             certificate):
@@ -66,9 +63,9 @@ class Arbitration(models.Model):
 
 
 class Jud(models.Model):
-    adress = models.CharField(max_length=64)
-    name = models.CharField(max_length=64)
-    judge_name = models.CharField(max_length=64)
+    adress = models.CharField(verbose_name="Адреса суду", max_length=64)
+    name = models.CharField(verbose_name="Назва суду", max_length=64)
+    judge_name = models.CharField(verbose_name="ПІБ суді", max_length=64)
 
     def add(self, adress, name, judge_name):
         self.adress = adress
@@ -78,14 +75,14 @@ class Jud(models.Model):
 
 
 class Person(models.Model):
-    type_exist = models.BooleanField(default=True)
-    name = models.CharField(max_length=64)
-    code = models.CharField(max_length=64)
-    ownership_part = models.IntegerField()
-    kved = models.CharField(max_length=64)
-    goverment_part = models.IntegerField()
-    jud_procedure = models.CharField(max_length=64)
-    information = models.TextField()
+    type_exist = models.BooleanField(verbose_name="Юридична особа", default=True)
+    name = models.CharField(verbose_name="ПІБ", max_length=64)
+    code = models.CharField(verbose_name="Код особи", max_length=64)
+    ownership_part = models.IntegerField(verbose_name="Частина власності")
+    kved = models.CharField(verbose_name="КВЕД", max_length=64)
+    goverment_part = models.IntegerField(verbose_name="Відділ уряду")
+    jud_procedure = models.CharField(verbose_name="Деталі процесу у суді", max_length=64)
+    information = models.TextField(verbose_name="Інформація по справі")
 
     def __str__(self):
         return self.name
@@ -103,17 +100,18 @@ class Person(models.Model):
 
 
 class Act(models.Model):
-    start_date = models.DateField()
-    finish_jud_date = models.DateField()
-    info_processing = models.TextField()
-    end_date = models.DateField()
-    arbitration = models.ForeignKey(Arbitration, on_delete=models.CASCADE)
-    jud_id_judge = models.ForeignKey(Jud)
-    person = models.ForeignKey(Person)
-    arbitr_status = models.CharField(max_length=64)
-    arbitr_start = models.CharField(max_length=64)
-    list_creditors = models.TextField()
-    creditor_requirements = models.TextField()
+    is_active = models.BooleanField(default=True)
+    start_date = models.DateField(verbose_name="Початкова дата")
+    finish_jud_date = models.DateField(verbose_name="Дата завершення справи у суді")
+    info_processing = models.TextField(verbose_name="Інформація, щодо процесу")
+    end_date = models.DateField(verbose_name="Дата завершення срави")
+    arbitration = models.ForeignKey(Arbitration, on_delete=models.DO_NOTHING, verbose_name="Арбітражний керуючий")
+    jud = models.ForeignKey(Jud, on_delete=models.CASCADE)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    arbitr_status = models.CharField(verbose_name="Статус арбітражного керуючого", max_length=64)
+    arbitr_start = models.DateField(verbose_name="Дата початку роботи")
+    list_creditors = models.TextField(verbose_name="Список кредиторів")
+    creditor_requirements = models.TextField(verbose_name="Вимоги кредиторів")
 
     def add(self, start_date, finish_jud_date, info_processing, end_date, arbitr_id, jud_id_date, person, arbitr_status,
             arbitr_start, list_creditors, creditor_requirements):
